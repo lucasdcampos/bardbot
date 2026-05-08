ca787a2a626729f52912c2c5c63427745b96cc57
Lucas M. Campos <lucas@lucasof.com>
2026-05-08 03:56:56 -0300

    chore: add youtube cookies

--- utils/downloader.py
+++ utils/downloader.py
@@ -14,7 +14,8 @@ YTDL_OPTIONS = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0', # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0',
+    'cookiefile': 'cookies.txt' if os.path.exists('cookies.txt') else None,
 }
 
 ffmpeg_options = {
